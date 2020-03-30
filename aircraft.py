d2340ef8f8b74e65c3f242d4a9d3c8d94e79f543
NoahSchwalb <ns0048@uah.edu>
2020-03-30 16:53:34 -0500

    Fix merge issues
    State class added, variable calcuations added in Aircraft class

--- aircraft.py
+++ aircraft.py
@@ -1,12 +1,38 @@
 import initial_conditions as initial
-import flight_data as data
+import flight
 import math as m
+import numpy as np
 
 class Aircraft():
-    def __init__(self):
-        self.v_max = data.v_max_flight()
-        self.E_max = data.E_max_flight()
-    
-    def C_L(self,rho):
-        C_L = (2*(self.v_max.v_max/m.log(initial.W_i/initial.W_f))*m.sqrt(2/rho/initial.S)*(initial.W_i**0.5-initial.W_f**0.5))**2
+    """
+	Module to define an aircraft
+
+	Units: English
+
+	Example:
+		aircraft = Aircraft()
+
+	"""
+    def C_L(self,rho,velocity):
+        # Calculate the C_L at any given rho
+        C_L = (2*(velocity/m.log(initial.W_i/initial.W_f))*m.sqrt(2/rho/initial.S)*(initial.W_i**0.5-initial.W_f**0.5))**2
         return C_L
+
+    def oswaldEfficiency(self,AR):
+        # Calculate the oswald efficiency factor for any given aspect ratio
+        oswald = 4.61*(1-0.045*AR**0.68)*(m.cos(initial.omega)**0.15)-3.1
+        return oswald
+
+    def C_D_i(self,C_L,e,AR):
+        # Calculate the C_D_i at any given coefficient of lift, oswald efficiency factor, and aspect ratio
+        C_D_i = C_L**2/(m.pi*e*AR)
+        return C_D_i
+
+    def C_D(self,C_D_i,C_D_o):
+        # Calculate the total C_D at any given C_D_i and C_D_o
+        C_D = C_D_i+C_D_o
+        return C_D
+
+    #def range(self,rho,ct,C_L,C_D,W_i,W_f):
+    #    range = 1
+    #    return range
---
+++ flight.py
@@ -0,0 +1,90 @@
+import initial_conditions as initial
+from aircraft import Aircraft
+import math as m
+
+class Flight(Aircraft):
+	"""
+	Module to define the flight segment of an aircraft mission
+
+	Units: English
+
+	Example:
+		import initial_conditions as initial
+
+		fuel = 1*initial.V_fuel_max
+		W_add = 0
+		t_alt = 400
+		rho_alt = 0.0001234
+		velocity = 0.5
+		rnge = 1000
+		flight = Flight(fuel,W_add,t_alt,rho_alt,velocity,rnge)
+
+	Inputs:
+	 1. fuel  - weight of fuel as function of max fuel
+	 2. W_add - additional weight added to the aircraft
+	 3. t 	  - temperature at the altitude for the flight
+	 4. rho   - density at the altitude for the flight
+	 5. v 	  - velocity as a function of mach number
+	 6. rnge  - range of the aircraft for the flight
+	 
+	"""
+	def __init__(self,fuel,W_add,t_alt,rho_alt,v,rnge):
+		self.fuel 	 = 	fuel					 		   				   # [lbf] 			 # [SOW]
+		self.W_add 	 = 	W_add 										 	   # [lbf] 			 # [SOW]
+		self.t_alt 	 = 	t_alt									 		   # [R] 			 # [Table]
+		self.rho_alt = 	rho_alt 								 		   # [slugs/ft^3] 	 # [Table]
+		self.mu 	 = 	2.27*10**-8*((self.t_alt**1.5)/(self.t_alt+198.6)) # [lb-s/ft^2] 	 # [FS]
+		self.gamma 	 = 	1.6 									 		   # [-] 			 # [-]
+		self.R		 = 	1716 									 		   # [ft-lbf/slug-R] # [FS]
+		self.mach 	 = 	(self.gamma*self.R*self.t_alt)**0.5 			   # [ft/s] 		 # [FS]
+		self.v 	 	 = 	v*self.mach  							 	   	   # [ft/s] 		 # [SOW]
+		self.rnge 	 =  rnge											   # [mi] 			 # [SOW]
+
+
+class State():
+	def __init__(self,temp):
+		self.gamma 	 = 	1.6 									 		   # [-] 			 # [-]
+		self.R		 = 	1716 									 		   # [ft-lbf/slug-R] # [FS]
+		self.rho_sea = 	0.002377										   # [slugs/ft^3]	 # [FS]
+		self.t_sea 	 = 	518.67											   # [R] 			 # [FS]
+		self.mu_sea  = 	2.27*10**-8*((self.t_sea**1.5)/(self.t_sea+198.6)) # [lb-s/ft^2] 	 # [FS]
+
+	def vAlt2Sea(self,rho,v):
+		sea = v*m.sqrt(rho/self.rho_sea)
+		return sea
+
+	def vSea2Alt(self,rho,v):
+		alt = v*m.sqrt(self.rho_sea/rho)
+		return alt
+
+"""
+## Max velocity flight
+class v_max_flight(Aircraft):
+	def __init__(self):
+		self.fuel 	 = 	0.5*initial.V_fuel_max 					 		   # [lbf] 			 # [SOW]
+		self.W_add 	 = 	0 										 		   # [lbf] 			 # [SOW]
+		self.alt 	 = 	28000 									 		   # [ft] 			 # [SOW]
+		self.t_alt 	 = 	418.97 									 		   # [R] 			 # [Table]
+		self.rho_alt = 	0.00095801 								 		   # [slugs/ft^3] 	 # [Table]
+		self.mu 	 = 	2.27*10**-8*((self.t_alt**1.5)/(self.t_alt+198.6)) # [lb-s/ft^2] 	 # [FS]
+		self.gamma 	 = 	1.6 									 		   # [] 			 # []
+		self.R		 = 	1716 									 		   # [ft-lbf/slug-R] # [FS]
+		self.mach 	 = 	(self.gamma*self.R*self.t_alt)**0.5 			   # [ft/s] 		 # [FS]
+		self.v_max 	 = 	0.801*self.mach  							 	   # [ft/s] 		 # [SOW]
+		self.range 	 = 	175.8 									 		   # [mi] 			 # [SOW]
+
+## Max endurance flight
+class E_max_flight(Aircraft):
+	def __init__(self):
+		self.fuel 	 = 	initial.V_fuel_max 						 		   # [lbf] 			 # [SOW]
+		self.W_add 	 = 	30000 									 		   # [lb] 			 # [SOW]
+		self.alt 	 = 	15000 									 		   # [ft] 			 # [SOW]
+		self.t_alt 	 = 	465.23 									 		   # [R] 			 # [Table]
+		self.rho_alt = 	0.0014962 										   # [slugs/ft^3] 	 # [Table]
+		self.mu 	 = 	2.27*10**-8*((self.t_alt**1.5)/(self.t_alt+198.6)) # [lb-s/ft^2] 	 # [FS]
+		self.gamma 	 = 	1.6 									 		   # [] 			 # []
+		self.R 		 = 	1716 									 		   # [ft-lbf/slug-R] # [FS]
+		self.mach	 = 	(self.gamma*self.R*self.t_alt)**0.5 			   # [] 			 # [FS]
+		self.E_max	 = 	0.9283 									 		   # [hr] 			 # [SOW]
+		self.v_inf 	 = 	0.4127*self.mach 								   # [] 			 # [SOW]
+"""

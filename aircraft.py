0742d7d359c8cd872f0dcf5d514a1998062c62f7
NoahSchwalb <ns0048@uah.edu>
2020-03-30 20:29:11 -0500

    Delete Aircraft constructor following updates to Flight class, update documentation

--- aircraft.py
+++ aircraft.py
@@ -4,13 +4,16 @@ import math as m
 import numpy as np
 
 class Aircraft():
-    def __init__(self):
-        # Module to create an aircraft
-        #  - v_max: creates object of v_max_flight with conditions from the max velocity flight
-        #  - E_max: creates object of E_max_flight with conditions from the max endurance flight
-        self.v_max = data.v_max_flight()
-        self.E_max = data.E_max_flight()
-    
+    """
+	Module to define an aircraft
+
+	Units: English
+
+	Example:
+		aircraft = Aircraft()
+
+	 
+	"""
     def C_L(self,rho,velocity):
         # Calculate the C_L at any given rho
         C_L = (2*(velocity/m.log(initial.W_i/initial.W_f))*m.sqrt(2/rho/initial.S)*(initial.W_i**0.5-initial.W_f**0.5))**2

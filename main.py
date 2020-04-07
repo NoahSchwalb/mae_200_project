231fff21d1b4421365dc04f00cb6f8c7690f3d38
NoahSchwalb <ns0048@uah.edu>
2020-04-06 19:21:52 -0500

    Fixed delvaration of v, fixed input parameter for C_L function, lowered number of values in v_sl list

--- main.py
+++ main.py
@@ -16,21 +16,28 @@ def mae_200_project():
     rho_alt  = 	0.0014962 										   # [slugs/ft^3] 	 # [Table]
     mu 	     = 	2.27*10**-8*((t_alt**1.5)/(t_alt+198.6)) # [lb-s/ft^2] 	 # [FS]
     mach	 = 	(gamma*R*t_alt)**0.5 			   # [] 			 # [FS]
-    E	 = 	0.9283*3600 									 		   # [s] 			 # [SOW]
-    v 	     = 	0.4127*mach 								   # [] 			 # [SOW]
+    E	     = 	0.9283*3600 									 		   # [s] 			 # [SOW]
+    v 	     = 	0.4127 								   # [ft/s] 			 # [SOW]
     rnge     =  ''
 
     
     E_max_flight = Flight(fuel,W_add,t_alt,rho_alt,v,rnge,E)
-    C_L = aircraft.CL(rho_alt,v,E_max_flight.W_i)
+    C_L = aircraft.CL(rho_alt,E_max_flight.v,E_max_flight.W_i)
     e = aircraft.oswaldEfficiency(initial.AR_front)
     C_D_i = aircraft.CDi(C_L,e,initial.AR_front)
     C_D_o = C_D_i #max endurance condition
     C_D = aircraft.CD(C_D_i,C_D_o)
     c_t = aircraft.ct(E_max_flight.E,C_L,C_D,E_max_flight.W_i,E_max_flight.W_f)
     rnge = aircraft.rnge(rho_alt,initial.S,c_t,C_L,C_D,E_max_flight.W_i,E_max_flight.W_f) #range for the max endurance flight data
-    
-    v_sl = np.linspace(0,1.0,500)
+
+    #print(C_D_i)
+    #print(C_L**2/(m.pi*e*initial.AR_front))
+    #print(C_D_o)
+    #print(C_D)
+    print(E_max_flight.E)
+    print(1/E_max_flight.E*C_L/C_D*m.log(E_max_flight.W_i/E_max_flight.W_f))
+    print(c_t*3600)
+    v_sl = np.linspace(0,1.0,10)
     v_sl.tolist()
     length = len(v_sl)
     W_add = initial.W_max-initial.V_fuel_max*initial.sigma_fuel-initial.W_f #additional weight needed to reach max possible weight
@@ -39,13 +46,31 @@ def mae_200_project():
     C_D_i_sl = aircraft.CDi_vector(C_L_sl,e,initial.AR_front,length)
     C_D_o_sl = C_D_i_sl
     C_D_sl = aircraft.CD_vector(C_D_i_sl,C_D_o_sl,length)
+    #print(C_L_sl)
+    
+    #print(aircraft.state.rho_sea)
+    #print(E_sl_flight.rho_alt)
+    #print(v_sl)
+    #print(E_sl_flight.v)
+
+    #for i,value in enumerate(v_sl):
+    #    print(2*E_sl_flight.W_i/(E_sl_flight.rho_alt*E_sl_flight.v[i]**2*initial.S))
+    #print(C_L_sl)
+    #for i,value in enumerate(C_L_sl):
+    #    print(C_L_sl[i]**2/(m.pi*e*initial.AR_front))
+    #print(C_D_i_sl)
+    #print(C_D_o_sl)
+    #print(C_D_sl)
     E_sl = aircraft.endurance_vector(c_t,C_L_sl,C_D_sl,E_sl_flight.W_i,E_sl_flight.W_f,length)
-    for i,value in enumerate(E_sl):
-        if value == 'nan':
-            E_sl[i] = 0
+    for i,value in enumerate(v_sl):
+        print(1/c_t*C_L_sl[i]/C_D_sl[i]*m.log(E_sl_flight.W_i/E_sl_flight.W_f))
+    
+    #for i,value in enumerate(E_sl):
+    #    if value == 'nan':
+    #        E_sl[i] = 0
     print(E_sl)
-    plt.plot(v_sl,E_sl/3600)
-    plt.show()
+    #plt.plot(v_sl,E_sl/3600)
+    #plt.show()
     #print(C_L_sl[300])
     #print(C_D_sl[300])
     #print(v_sl[300])

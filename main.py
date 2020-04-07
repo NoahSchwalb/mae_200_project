0ddeab5d92ae214127aa0f0622efb3459eda210c
NoahSchwalb <ns0048@uah.edu>
2020-04-07 01:34:09 -0500

    Fix range for V_max_flight, add code to find C_D_o, calculate the maximum endurance at sea level

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def mae_200_project():
     mach	 = 	(gamma*R*t_alt)**0.5 			   # [] 			 # [FS]
     E	     = 	''									 		   # [s] 			 # [SOW]
     v 	     = 	0.801 								   # [ft/s] 			 # [SOW]
-    rnge     =  175.8
+    rnge     =  175.8*5280
 
     V_max_flight = Flight(fuel,W_add,t_alt,rho_alt,v,rnge,E)
 
@@ -35,67 +35,47 @@ def mae_200_project():
 
     E_max_flight = Flight(fuel,W_add,t_alt,rho_alt,v,rnge,E)
 
-    
-
+    # Find c_t using E_max_flight
     C_L = aircraft.CL(rho_alt,E_max_flight.v,E_max_flight.W_i)
     e = aircraft.oswaldEfficiency(initial.AR_front)
     C_D_i = aircraft.CDi(C_L,e,initial.AR_front)
     C_D_o = C_D_i #max endurance condition
     C_D = aircraft.CD(C_D_i,C_D_o)
     c_t = aircraft.ct(E_max_flight.E,C_L,C_D,E_max_flight.W_i,E_max_flight.W_f)
-    rnge = aircraft.rnge(rho_alt,initial.S,c_t,C_L,C_D,E_max_flight.W_i,E_max_flight.W_f) #range for the max endurance flight data
 
-    #print(C_D_i)
-    #print(C_L**2/(m.pi*e*initial.AR_front))
-    #print(C_D_o)
-    #print(C_D)
-    #print(E_max_flight.E)
-    #print(1/E_max_flight.E*C_L/C_D*m.log(E_max_flight.W_i/E_max_flight.W_f))
-    #print(c_t*3600)
-    v_sl = np.linspace(0,1.0,10)
+    # Find C_D_o using V_max flight
+    C_L = aircraft.CL(V_max_flight.rho_alt,V_max_flight.v,V_max_flight.W_i)
+    C_D_i = aircraft.CDi(C_L,e,initial.AR_front)
+    C_D_o = aircraft.CDo(V_max_flight.rho_alt,initial.S,c_t,C_L,C_D_i,V_max_flight.W_i,V_max_flight.W_f,V_max_flight.rnge)
+
+
+    v_sl = np.linspace(0.1,1.0,10000)
     v_sl.tolist()
     length = len(v_sl)
-    C_D_o_sl = np.zeros(length)
-    for value in C_D_o_sl:
-        value = 0.0648
+
     W_add = initial.W_max-initial.V_fuel_max*initial.sigma_fuel-initial.W_f #additional weight needed to reach max possible weight
     E_sl_flight = Flight(fuel,W_add,aircraft.state.t_sea,aircraft.state.rho_sea,v_sl,'','')
     C_L_sl = aircraft.CL_vector(aircraft.state.rho_sea,E_sl_flight.v,E_sl_flight.W_i,length)
     C_D_i_sl = aircraft.CDi_vector(C_L_sl,e,initial.AR_front,length)
-    #C_D_o_sl = C_D_i_sl
-    #print(C_D_i_sl)
-    C_D_sl = aircraft.CD_vector(C_D_i_sl,C_D_o_sl,length)
-    #C_D_sl = [0.09249552242870, 0.09249552242870, 0.09249552242870, 0.09249552242870, 0.09249552242870, 0.09249552242870, 0.09249552242870, 0.09249552242870, 0.09249552242870, 0.09249552242870]
-    #for i,value in enumerate(C_L_sl):
-    #    print('CL: ' + str(value) + '   CD: ' + str(C_D_sl[i]) + '    CL/CD: ' + str(value/C_D_sl[i]))
-
-    #for i,value in enumerate(v_sl):
-    #    print(2*E_sl_flight.W_i/(E_sl_flight.rho_alt*E_sl_flight.v[i]**2*initial.S))
-    #print(C_L_sl)
-    #for i,value in enumerate(C_L_sl):
-    #    print(C_L_sl[i]**2/(m.pi*e*initial.AR_front))
-    #print(C_D_i_sl)
-    #print(C_D_o_sl)
-    #print(C_D_sl)
+    C_D_sl = aircraft.CD_vector(C_D_i_sl,C_D_o,length)
     E_sl = aircraft.endurance_vector(c_t,C_L_sl,C_D_sl,E_sl_flight.W_i,E_sl_flight.W_f,length)
-    #for i,value in enumerate(v_sl):
-    #    print(1/c_t*C_L_sl[i]/C_D_sl[i]*m.log(E_sl_flight.W_i/E_sl_flight.W_f))
+    E_max_sl = 0
+    for i,value in enumerate(E_sl):
+        if value > E_max_sl:
+            E_max_sl = value
+            E_v_max_sl = E_sl_flight.v[i]
     
-    #for i,value in enumerate(E_sl):
-    #    if value == 'nan':
-    #        E_sl[i] = 0
-    print(E_sl/3600)
-    #print(E_sl_flight.v)
-    #print(C_D_sl)
-    #print(C_L_sl)
-    #plt.plot(v_sl,E_sl/3600)
-    #plt.show()
-    #print(C_L_sl[300])
-    #print(C_D_sl[300])
-    #print(v_sl[300])
-    #print(E_sl_flight.W_i)
-    #print(E_sl_flight.W_f)
-    #print(E_sl[300]/3600)
+    print('The maximum endurance at sea level is ' + str(E_max_sl) + ' hr and the speed required to achieve that endurance is ' + str(E_v_max_sl) + ' ft/s.\n')
+    #print('The maximum range at sea level is ' + str(R_max_sl) + ' hr and the speed required to achieve that endurance is ' + str(R_v_max_sl) + ' ft/s.\n')
+
+
+    # Plot looks about right
+    plt.plot(E_sl_flight.v,E_sl/3600)
+    plt.plot(E_v_max_sl,E_max_sl/3600,'r*')
+    plt.xlabel('Velcoity [ft/s]')
+    plt.ylabel('Endurance [hr]')
+    plt.title('Endurance vs Velocity at Sea Level')
+    plt.show()
 
     
 
